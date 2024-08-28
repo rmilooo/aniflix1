91e4f142366240335b82b69c2ca6a0b09948d3c1
rmilooo <73422068+rmilooo@users.noreply.github.com>
2024-08-28 22:59:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function getCookie(name) {
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(';').shift();
   }
+  window.alert(getCookie('logged_in'));
   
   // Check if user is logged in by looking for the "logged_in" cookie
   if (getCookie("logged_in") !== "true") {
